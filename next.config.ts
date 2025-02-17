a8ff56f42b154cb230c4b6dfc3dec2c53749bedc
Kaung Htet Soe <prime@Primes-MacBook-Air.local>
2025-02-17 18:21:28 +0630

    fix export config

--- next.config.ts
+++ next.config.ts
@@ -6,8 +6,8 @@ const nextConfig: NextConfig = {
   // If you are deploying to a project page on GitHub (e.g., github.com/<username>/<repo>):
   // 1. Uncomment and update the basePath below with your repo name.
   // 2. Make sure you also update "assetPrefix" if needed.
-  basePath: 'https://github.com/kaunghtetsoe/kaunghtetsoe.github.io',
-  assetPrefix: 'https://github.com/kaunghtetsoe/kaunghtetsoe.github.io/',
+  // basePath: 'https://github.com/kaunghtetsoe/kaunghtetsoe.github.io',
+  // assetPrefix: 'https://github.com/kaunghtetsoe/kaunghtetsoe.github.io/',
 };
 
 export default nextConfig;
